54ea5c2be24afd5e4fb207c5380d12e91cedacd9
Olivier Bazoud <olivier.bazoud@gmail.com>
2016-07-05 13:29:52 +0200

    Add ec2_node

--- attributes/handlers.rb
+++ attributes/handlers.rb
@@ -2,3 +2,5 @@ default['monitor']['pagerduty_api_key'] = ''
 
 default['monitor']['graphite_address'] = nil
 default['monitor']['graphite_port'] = nil
+
+default['monitor']['ec2_states'] = ['terminated','stopping','shutting-down','stopped']
